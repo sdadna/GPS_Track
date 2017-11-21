8c2634a7255d836feb5778f4669ff86690951f68
sdadna <sdadna1992@163.com>
2017-11-21 15:51:27 +0800

    modify track.html to draw line by data

---
+++ DrawTrack.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import urllib
+import os
+import json
+from BaseHTTPServer import HTTPServer,BaseHTTPRequestHandler
+import SocketServer
+import thread,time
+
+i = 0;
+data = [{'jingdu':117.27,'weidu':31.86},{'jingdu':117.16,'weidu':32.47},{'jingdu':116.98,'weidu':32.62},{'jingdu':118.38,'weidu':31.33}]
+#data = {}
+class HTTPHandler(BaseHTTPRequestHandler):
+
+	# def do_GET(self):
+	# 	self.send_response(200)
+	# 	self.send_header('Content-type', 'text/html')
+	# 	self.end_headers()
+
+	# 	self.wfile.write("hello world!")
+	def do_GET(self):
+		if self.path == '/':
+			self.path = 'track.html'
+			f = open(self.path)
+			
+			self.send_response(200)
+			self.send_header('Content-type', 'text/html')
+			self.end_headers()
+			self.wfile.write(f.read())
+			f.close()
+		elif self.path == '/track.ico':
+			f = open('track.ico')
+			self.send_response(200)
+			self.send_header('Content-type', 'text/html')
+			self.end_headers()
+			self.wfile.write(f.read())
+			f.close()
+		elif self.path == '/GPSdata':
+		#	data = {'jingdu':12,'weidu':35} #dict 
+			global i		
+			if(len(data))
+				json_str = json.dumps(data[i])
+				i = (i + 1) % 4
+			print "json:",json_str
+
+			self.send_response(200)
+			self.send_header('Content-type', 'text/html')
+			self.end_headers()
+			self.wfile.write(json_str)
+
+		return
+
+class localHostServer(SocketServer.BaseRequestHandler):
+
+		def handle(self):
+			data = {}
+			conn = self.request
+			ret_bytes = conn.recv(1024)
+			data["jingdu"] = 110
+			data["weidu"] = 34
+			print ret_str
+
+def start_server(port):
+	http_server = HTTPServer(('', int(port)), HTTPHandler)
+	http_server.serve_forever()
+
+
+def start_local_server(port):
+	server_sk = SocketServer.ThreadingTCPServer(("127.0.0.1", int(port)), localHostServer)
+	server_sk.serve_forever()
+
+try:
+	thread.start_new_thread(start_server,(8000,)) #http server
+	thread.start_new_thread(start_local_server,(9000,)) #local host server
+except:
+	print "Error unable create multi theread"
+
+while 1:
+	pass
---
+++ socket.c
@@ -0,0 +1,36 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+int client(){
+	int client_sock;
+	int ret;
+	struct sockaddr_in serverAddr;
+
+	if((client_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		perror("socket");
+		return 1;
+	}
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(9000);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	if(connect(client_sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0){
+		perror("connect");
+		return 1;
+	}
+
+	char ch[] = "1235";
+	ret  = write(client_sock, ch, strlen(ch) + 1);
+	printf("%d %s\n",ret, ch);
+	close(client_sock);
+}
+
+int main(int argc, char const *argv[])
+{
+	client();
+	return 0;
+}
